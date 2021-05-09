c49d6e53ddc9f6a7b0e380c7c191d384a0f9e904
leechoncaao <hathaykhongbanghayhat18@gmail.com>
2021-05-09 10:12:33 +0700

    push source code BE

---
+++ BE/src/main/java/com/auction_website/service/product/ProductService.java
@@ -0,0 +1,4 @@
+package com.auction_website.service.product;
+
+public interface ProductService {
+}
---
+++ BE/src/main/java/com/auction_website/service/product/ProductServiceImpl.java
@@ -0,0 +1,11 @@
+package com.auction_website.service.product;
+
+import com.auction_website.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+    @Autowired
+    private ProductRepository productRepository;
+}
